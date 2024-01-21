d2d603e0056f31740ba1c5bf02c82159086203ea
Rosie <rosieyxl@gmail.com>
2024-01-21 12:19:41 -0500

    working maze code

--- maze.ino
+++ maze.ino
@@ -16,6 +16,9 @@ const int echo = 11;
 long duration;
 int distance;
 
+// declare a boolean variable
+  boolean hitWallAlr = false;
+
 void setup() {
   
   // Set the motor control pins as outputs
@@ -26,22 +29,16 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
-  // Enable PWM for Motor A and Motor B
-  analogWrite(enA, 200);  // Adjust the speed for Motor A as needed
-  analogWrite(enB, 200);  // Adjust the speed for Motor B as needed
-
   pinMode(trig, OUTPUT); // sets the trig pin as output
   pinMode(echo, INPUT); // sets the echo pin as input
   Serial.begin(9600); // starts the serial communication
 
-  // declare a boolean variable
-  boolean hitWallAlr = false;
 }
 
 void loop() {
   // clear any previous input
   digitalWrite(trig, LOW);
-  delay(1000); // wait 1s between outputs
+  delayMicroseconds(2);
 
   // set trig pin to HIGH for 10 microseconds
   digitalWrite(trig, HIGH);
@@ -53,21 +50,18 @@ void loop() {
   // calculate the distance (in cm)
   distance = duration*0.034/2;
 
-  if (distance <= 30 && !hitWallAlr) {
+  if (distance <= 20 && !hitWallAlr) {
+    stop();
     rotateMotors(250,100);
-    delay(100); // ADJUST AFTER TESTING TMR
     hitWallAlr = !hitWallAlr;
   } else if (distance <= 30 && hitWallAlr) {
+    stop();
     rotateMotors(100, 250);
-    delay(200);
     hitWallAlr = !hitWallAlr;
   } else {
     moveMotors(200);
   }
 
-  // print the distance on serial monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
 }
 
 void moveMotors(int speed) {
@@ -81,7 +75,7 @@ void moveMotors(int speed) {
   analogWrite(enB, speed);
 }
 
-/*void stop() {
+void stop() {
   // Stop both motors
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
@@ -90,7 +84,7 @@ void moveMotors(int speed) {
   // Set speed to 0 to stop the motors
   analogWrite(enA, 0);
   analogWrite(enB, 0);
-}*/
+}
 
 void rotateMotors(int speedA, int speedB) {
   // Rotate motors in opposite directions to achieve rotation
